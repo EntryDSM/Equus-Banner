d022658b514f10d4f49e60721967f165b6fd1742
도경 <dokyungkim2926@gmail.com>
2023-11-13 09:04:15 +0900

    create :: project

---
+++ src/main/kotlin/com/example/equusbanner/EquusBannerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.equusbanner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EquusBannerApplication
+
+fun main(args: Array<String>) {
+	runApplication<EquusBannerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/equusbanner/EquusBannerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.equusbanner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EquusBannerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
