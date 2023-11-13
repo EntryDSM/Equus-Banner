fc290ee2f2c2d04cb746738e280b2982c7af5fd5
도경 <dokyungkim2926@gmail.com>
2023-11-13 10:40:53 +0900

    setting ::buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,21 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.5"
-	id("io.spring.dependency-management") version "1.1.3"
-	kotlin("jvm") version "1.8.22"
-	kotlin("plugin.spring") version "1.8.22"
+	id("org.springframework.boot") version PluginVersions.SPRING_BOOT_VERSION
+	id("io.spring.dependency-management") version PluginVersions.DEPENDENCY_MANAGER_VERSION
+	id("org.jlleitschuh.gradle.ktlint") version PluginVersions.KLINT_VERSION
+	kotlin("jvm") version PluginVersions.JVM_VERSION
+	kotlin("plugin.spring") version PluginVersions.SPRING_PLUGIN_VERSION
+	kotlin("plugin.jpa") version PluginVersions.JPA_PLUGIN_VERSION
 }
 
-group = "com.example"
+dependencyManagement {
+	imports {
+		mavenBom(Dependencies.SPRING_CLOUD)
+	}
+}
+
+group = "hs.kr.equus"
 version = "0.0.1-SNAPSHOT"
 
 java {
@@ -16,12 +24,34 @@ java {
 
 repositories {
 	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	// Database
+	implementation(Dependencies.SPRING_DATA_JPA)
+	runtimeOnly(Dependencies.MYSQL_CONNECTOR)
+
+	// Web
+	implementation(Dependencies.SPRING_WEB)
+
+	// Security
+	implementation(Dependencies.SPRING_SECURITY)
+
+	// Kotlin
+	implementation(Dependencies.JACKSON)
+	implementation(Dependencies.KOTLIN_REFLECT)
+
+	// Test
+	testImplementation(Dependencies.SPRING_TEST)
+
+	// Valid
+	implementation(Dependencies.SPRING_VALIDATION)
+
+	// Cloud Config
+	implementation(Dependencies.CLOUD_CONFIG)
+
 }
 
 tasks.withType<KotlinCompile> {
@@ -33,8 +63,4 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
-}
-
-tasks.bootBuildImage {
-	builder.set("paketobuildpacks/builder-jammy-base:latest")
 }
---
+++ buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -0,0 +1,6 @@
+object DependencyVersions {
+    const val SPRING_CLOUD_VERSION = "2021.0.1"
+    const val JACKSON_VERSION = "2.10.3"
+    const val MYSQL = "8.0.28"
+    const val JSON_VERSION = "20200518"
+}
