942c8865c7d62f15de1bc56bbfca150e516bbaa9
도경 <dokyungkim2926@gmail.com>
2023-11-16 19:09:46 +0900

    refactor :: change UUID -> Long

--- src/main/kotlin/hs/kr/equus/banner/domain/banner/domain/repository/BannerLinkRepository.kt
+++ src/main/kotlin/hs/kr/equus/banner/domain/banner/domain/repository/BannerLinkRepository.kt
@@ -4,5 +4,5 @@ import hs.kr.equus.banner.domain.banner.domain.BannerLink
 import org.springframework.data.repository.CrudRepository
 import java.util.UUID
 
-interface BannerLinkRepository : CrudRepository<BannerLink, UUID> {
+interface BannerLinkRepository : CrudRepository<BannerLink, Long> {
 }
