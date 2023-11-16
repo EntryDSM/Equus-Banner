9435bdf051ee5be096b04d3dbf9cde75acc5aeeb
도경 <dokyungkim2926@gmail.com>
2023-11-16 17:40:34 +0900

    fix :: image 오류 해결

--- src/main/kotlin/hs/kr/equus/banner/global/utils/S3Utils.kt
+++ src/main/kotlin/hs/kr/equus/banner/global/utils/S3Utils.kt
@@ -2,9 +2,7 @@ package hs.kr.equus.banner.global.utils
 
 import com.amazonaws.HttpMethod
 import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.*
-import com.amazonaws.util.IOUtils
 import hs.kr.equus.banner.global.exception.BadFileExtensionException
 import hs.kr.equus.banner.global.exception.EmptyFileException
 import hs.kr.equus.banner.global.exception.ImageNotFoundException
@@ -24,40 +22,41 @@ import javax.imageio.ImageIO
 
 @Component
 class S3Utils(
-    private val amazonS3: AmazonS3,
-
     @Value("\${spring.cloud.aws.s3.bucket}")
     private val bucketName: String,
-
+    private val amazonS3: AmazonS3
 ) {
     companion object {
-        private val EXP_TIME: Int = 1000 * 60 * 2
+        private const val EXP_TIME = 1000 * 60 * 2
     }
 
-    fun upload(file: MultipartFile?, path: String): String {
+    fun upload(file: MultipartFile, path: String): String {
         val ext = verificationFile(file)
+
         val randomName = UUID.randomUUID().toString()
         val filename = "$randomName.$ext"
-        val outputImage: BufferedImage
+        val outputImage = makeThumbnail(file)
         val os = ByteArrayOutputStream()
-        outputImage = makeThumbnail(file!!)
-        val inStream: InputStream = ByteArrayOutputStream().run {
-            try {
-                ImageIO.write(outputImage, "png", this)
-            } catch (e: IOException) {
-                throw ImageNotFoundException
-            }
-            ByteArrayInputStream(toByteArray())
+
+        try {
+            ImageIO.write(outputImage, "png", os)
+        } catch (e: IOException) {
+            throw ImageNotFoundException
         }
+
+        val inputStream : InputStream = ByteArrayInputStream(os.toByteArray())
+
         val metadata = ObjectMetadata().apply {
             contentType = MediaType.IMAGE_PNG_VALUE
             contentLength = os.size().toLong()
             contentDisposition = "inline"
         }
+
         amazonS3.putObject(
-            PutObjectRequest(bucketName, path + filename, inStream, metadata)
-                .withCannedAcl(CannedAccessControlList.AuthenticatedRead)
+            PutObjectRequest(bucketName, "$path$filename", inputStream, metadata)
+            .withCannedAcl(CannedAccessControlList.AuthenticatedRead)
         )
+
         return filename
     }
 
@@ -66,63 +65,51 @@ class S3Utils(
             time = time + EXP_TIME.toLong()
         }
         val url: URL = amazonS3.generatePresignedUrl(
-            GeneratePresignedUrlRequest("", fileName)
+            GeneratePresignedUrlRequest(bucketName, fileName)
                 .withMethod(HttpMethod.GET)
                 .withExpiration(expiration)
         )
         return url.toString()
     }
 
-    fun getObject(fileName: String, path: String): ByteArray {
-        try {
-            val s3Object: S3Object = amazonS3.getObject(bucketName, "$path$fileName")
-            return IOUtils.toByteArray(s3Object.objectContent)
-        } catch (e: RuntimeException) {
-            throw ImageNotFoundException
-        }
-    }
 
     fun delete(objectName: String, path: String) {
-        amazonS3.deleteObject(bucketName, objectName + path)
+        amazonS3.deleteObject(bucketName, "$path$objectName")
     }
 
     private fun makeThumbnail(file: MultipartFile): BufferedImage {
-        val srcImg: BufferedImage
-        srcImg = try {
-            ImageIO.read(file.inputStream)
-        } catch (e: IOException) {
-            throw EmptyFileException
-        }
-        val dw = 300
-        val dh = 400
-        val ow = srcImg.width
-        val oh = srcImg.height
-        var nw = ow
-        var nh = ow * dh / dw
-        if (nh > oh) {
-            nw = oh * dw / dh
-            nh = oh
-        }
-        val cropImg: BufferedImage = Scalr.crop(srcImg, (ow - nw) / 2, (oh - nh) / 2, nw, nh)
-        return Scalr.resize(cropImg, dw, dh)
-    }
-
-    private fun makePdfThumbnail(file: MultipartFile): BufferedImage {
         val srcImg: BufferedImage
         try {
             srcImg = ImageIO.read(file.inputStream)
         } catch (e: IOException) {
             throw EmptyFileException
         }
-        return srcImg
+
+        val dw = 300
+        val dh = 400
+
+        val ow = srcImg.width
+        val oh = srcImg.height
+
+        var nw = ow
+        var nh = (ow * dh) / dw
+
+        if (nh > oh) {
+            nw = (oh * dw) / dh
+            nh = oh
+        }
+
+        val cropImg = Scalr.crop(srcImg, (ow - nw) / 2, (oh - nh) / 2, nw, nh)
+
+        return Scalr.resize(cropImg, dw, dh)
     }
 
-    private fun verificationFile(file: MultipartFile?): String {
-        if (file == null || file.isEmpty || file.originalFilename == null) throw EmptyFileException
+    private fun verificationFile(file: MultipartFile): String {
+        if (file.isEmpty || file.originalFilename == null) throw EmptyFileException
         val originalFilename = file.originalFilename!!
-        val ext = originalFilename.substring(originalFilename.lastIndexOf(".") + 1)
-        val lowerExt = ext.lowercase(Locale.getDefault())
-        if (!(lowerExt == "jpg" || lowerExt == "jpeg" || lowerExt == "png" || lowerExt == "heic")) throw BadFileExtensionException
+        val ext = originalFilename.substring(originalFilename.lastIndexOf(".") + 1).lowercase(Locale.getDefault())
+
+        if (!(ext == "jpg" || ext == "jpeg" || ext == "png" || ext == "heic")) throw BadFileExtensionException
         return ext
     }
 }
