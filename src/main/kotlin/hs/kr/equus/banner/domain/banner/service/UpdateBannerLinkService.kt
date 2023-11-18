543a0a9e79c34512791b2f2e77ee36009b61e48f
도경 <dokyungkim2926@gmail.com>
2023-11-18 21:17:35 +0900

    refactor :: logic

--- src/main/kotlin/hs/kr/equus/banner/domain/banner/service/UpdateBannerLinkService.kt
+++ src/main/kotlin/hs/kr/equus/banner/domain/banner/service/UpdateBannerLinkService.kt
@@ -14,9 +14,10 @@ class UpdateBannerLinkService(
     private val s3Utils: S3Utils
 ) {
     @Transactional
-    fun execute(file: MultipartFile, id: Long): String {
+    fun execute(file: MultipartFile, id: Long): String? {
         val bannerLink = bannerLinkRepository.findById(id).orElseThrow {BannerNotFoundException}
         val fileName = s3Utils.upload(file)
-        return bannerLink.update(fileName)
+        bannerLink.update(fileName)
+        return s3Utils.generateObjectUrl(fileName)
     }
 }
