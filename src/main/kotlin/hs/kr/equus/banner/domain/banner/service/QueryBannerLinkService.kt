5bad184d5397fae215a0f757b92ea10e24a70f98
도경 <dokyungkim2926@gmail.com>
2023-11-18 20:52:01 +0900

    refactor :: logic

--- src/main/kotlin/hs/kr/equus/banner/domain/banner/service/QueryBannerLinkService.kt
+++ src/main/kotlin/hs/kr/equus/banner/domain/banner/service/QueryBannerLinkService.kt
@@ -5,17 +5,11 @@ import hs.kr.equus.banner.domain.banner.domain.repository.BannerLinkRepository
 import hs.kr.equus.banner.global.utils.S3Utils
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.web.multipart.MultipartFile
 
 @Service
-class CreateBannerLinkService (
+class QueryBannerLinkService(
     private val bannerLinkRepository: BannerLinkRepository,
     private val s3Utils: S3Utils
 ) {
-    @Transactional
-    fun execute(file : MultipartFile): String? {
-        bannerLinkRepository.save(BannerLink(fileName = file.name))
-        s3Utils.upload(file)
-        return s3Utils.generateObjectUrl(file.name)
-    }
+
 }
