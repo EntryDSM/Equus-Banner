9c51145bcb505357cd0c2f399eed15b4a013f202
도경 <dokyungkim2926@gmail.com>
2023-11-20 22:08:22 +0900

    fix :: value값 변경 안되는거 해결

--- src/main/kotlin/hs/kr/equus/banner/domain/banner/service/UpdateBannerLinkService.kt
+++ src/main/kotlin/hs/kr/equus/banner/domain/banner/service/UpdateBannerLinkService.kt
@@ -3,7 +3,6 @@ package hs.kr.equus.banner.domain.banner.service
 import hs.kr.equus.banner.domain.banner.domain.repository.BannerLinkRepository
 import hs.kr.equus.banner.domain.banner.exception.BannerNotFoundException
 import hs.kr.equus.banner.global.utils.S3Utils
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.multipart.MultipartFile
@@ -15,8 +14,13 @@ class UpdateBannerLinkService(
 ) {
     @Transactional
     fun execute(file: MultipartFile, id: Long): String {
-        val bannerLink = bannerLinkRepository.findById(id).orElseThrow {BannerNotFoundException}
-        bannerLink.update(file.name)
-        return s3Utils.upload(file)
+        val bannerLink = bannerLinkRepository.findById(id)
+            .orElseThrow { BannerNotFoundException }
+
+        val newFileName = s3Utils.upload(file)
+        bannerLink.update(newFileName)
+
+        bannerLinkRepository.save(bannerLink)
+        return newFileName
     }
 }
